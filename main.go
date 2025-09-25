9041977fc1a87b2d765bd97b13d70c64d0287de2
UltraMagnusss <mahmudi200102@gmail.com>
2025-09-25 18:17:00 +0300

    Now we can creat the new account with 0 money in it< fo now we can only write the name of the holder< not the second or father's name, soon it will be fixed

--- main.go
+++ main.go
@@ -29,20 +29,30 @@ func Transfer(from *Account, to *Account, amount float64) bool {
 }
 
 func main() {
+
+	var accounts []Account
+
 	for { // Createed a cycle for an infinity menu
 		//Typing the menu
-		fmt.Println("Menu:")
+		fmt.Println("\n--- Bank Menu ---")
 		fmt.Println("1. Create account")
 		fmt.Println("2. Exit")
-
+		fmt.Print("What can I do for You?")
 		//awaiting the user's choice
+
 		var choice int
-		fmt.Println("Choose the action: ")
 		fmt.Scan(&choice) //reading the number, the user write
 
 		//analyzing the choice
 		if choice == 1 {
-			fmt.Println("Creating the new account...") //here will be code writen after a while
+			var name string
+			fmt.Println("Creating the new account...")
+			fmt.Println("Write your full name down")
+			fmt.Scan(&name)
+			acc := Account{Owner: name, Balance: 0} //created the new account
+			accounts = append(accounts, acc)        // added the created account to the slice
+
+			fmt.Printf("Account for %s successfuly created! Balance %2f\n", name, acc.Balance) // displaying the confirmation massage to the user
 		} else if choice == 2 {
 			fmt.Println("Good bye!")
 			break //exit from the cycle, program shut down
